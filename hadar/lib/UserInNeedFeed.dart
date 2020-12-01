91a7fbbc0c70737e2e0b70396c388db814b28273
HaneenJer <haneenjeries65@gmail.com>
2020-12-01 16:49:21 +0200

    Update UserInNeedFeed.dart

--- hadar/lib/UserInNeedFeed.dart
+++ hadar/lib/UserInNeedFeed.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -8,23 +7,83 @@ class helpWindow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('choose a category'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    final title = 'Help Categories';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
           children: [
-            ColoredBox(child: (Text('Hey') ), color: (Colors.amberAccent),),
-            RaisedButton(child: Text('groceries')),
-            RaisedButton(child: Text('Babysitter')),
-            RaisedButton(child: Text('Health issue')),
-            RaisedButton(child: Text('Dentist')),
-            RaisedButton(child: Text('Food')),
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                  side: BorderSide(color: Color.fromRGBO(0, 160, 227, 1))),
+              child: Text('groceries'),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DescriptonBox(title: 'Grocories'),
+                ),
+              ),
+            ),
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                  side: BorderSide(color: Color.fromRGBO(0, 160, 227, 1))),
+              child: Text('Babysitter'),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DescriptonBox(title: 'Babysitter'),
+                ),
+              ),
+            ),
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                  side: BorderSide(color: Color.fromRGBO(0, 160, 227, 1))),
+              child: Text('Health issue'),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DescriptonBox(title: 'Health issues'),
+                ),
+              ),
+            ),
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                  side: BorderSide(color: Color.fromRGBO(0, 160, 227, 1))),
+              child: Text('Dentist'),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DescriptonBox(title: 'Dentist'),
+                ),
+              ),
+            ),
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                  side: BorderSide(color: Color.fromRGBO(0, 160, 227, 1))),
+              child: Text('Food'),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DescriptonBox(title: 'Food'),
+                ),
+              ),
+            ),
           ],
         ),
-      ), backgroundColor: Colors.white70,
+      ),
     );
   }
 }
@@ -41,13 +100,127 @@ class UserInNeedFeed extends StatelessWidget {
       body: Center(
         child: RaisedButton(
           child: Text('I need help'),
-          onPressed: () =>
-          {
+          onPressed: () => {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => helpWindow()),
             )
           },
+        ),
+      ),
+    );
+  }
+}
+
+//when a user clicks on the category, he gets a description box,
+// where he can describe his request
+class DescriptonBox extends StatefulWidget {
+  DescriptonBox({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _DescriptonBox createState() => _DescriptonBox();
+}
+
+class _DescriptonBox extends State<DescriptonBox> {
+  String _inputtext = 'waiting..';
+  TextEditingController inputtextField = TextEditingController();
+
+  void _processText() {
+    setState(() {
+      _inputtext = inputtextField.text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: inputtextField,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Description'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                '$_inputtext',
+                style: Theme.of(context).textTheme.display1,
+              ),
+            ),
+            RaisedButton(
+              onPressed: _processText,
+              child: Text('Done'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//show/hide, not used yet..
+//when clicking on a post a slide sheet appears with the options of renew, and
+//track the request
+//will be used after the feed is implemented
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void slideSheet() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            color: Color(0xFF737373),
+            child: Container(
+              height: 180,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10)),
+                color: Colors.white,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[Text('Renew')],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[Text('Track')],
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: slideSheet,
+          child: Text('Click'),
         ),
       ),
     );
