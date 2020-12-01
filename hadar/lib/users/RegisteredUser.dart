c6cd511a396bca4c9606aa2300549b7582c20572
AbuJabal-Hussein <73711735+AbuJabal-Hussein@users.noreply.github.com>
2020-12-01 18:32:00 +0200

    created user_in_need feed
    created a feed sytem for the user_in_need with clickable items

--- hadar/lib/users/RegisteredUser.dart
+++ hadar/lib/users/RegisteredUser.dart
@@ -2,7 +2,8 @@
 import 'package:hadar/users/User.dart';
 
 abstract class RegisteredUser extends User{
+  bool isSignedIn;
 
-  RegisteredUser(String name, String phoneNumber, String email, Privilege privilege)
-    : super(name, phoneNumber, email, privilege);
+  RegisteredUser(String name, String phoneNumber, String email, Privilege privilege, bool isSignedIn)
+    : isSignedIn = isSignedIn, super(name, phoneNumber, email, privilege);
 }
--- hadar/lib/utils/HelpRequest.dart
+++ hadar/lib/utils/HelpRequest.dart
@@ -6,6 +6,14 @@ class HelpRequest{
   HelpRequestType category;
   String description;
   DateTime date;
+  static int indexer = 0;
 
   HelpRequest(this.sender, this.category, this.description, this.date);
+  HelpRequest.fake():
+        sender = "slim shady" + (indexer++).toString(),
+        category = HelpRequestType("food"),
+        description = "i need someone to peel some potatoes for me",
+        date = DateTime.now();
+
+  //todo: should add information about people who offered help and the status of the request
 }
