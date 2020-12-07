4d281ed7002c4074244afaeae8bed1a57f40d04d
SamiHam162 <73660804+SamiHam162@users.noreply.github.com>
2020-12-07 15:47:06 +0200

    Adding dialog

--- hadar/lib/feeds/feed_items/help_request_tile.dart
+++ hadar/lib/feeds/feed_items/help_request_tile.dart
@@ -1,52 +1,139 @@
 import 'package:flutter/material.dart';
 import 'package:hadar/feeds/helper_feed.dart';
 import 'package:hadar/utils/HelpRequest.dart';
+import 'package:intl/intl.dart';
 
-
-class HelpRequestTile extends StatelessWidget {
+class HelpRequestTile extends StatefulWidget {
 
   final HelpRequest helpRequest;
   HelpRequestTile({this.helpRequest});
-  var _count = 0;
-  var _tapPosition;
+
+  @override
+  _HelpRequestTileState createState() => _HelpRequestTileState();
+}
+
+
+
+
+class _HelpRequestTileState extends State<HelpRequestTile> {
+
+
+  _HelpRequestTileState();
+  final DateTime now = DateTime.now();
+  final DateFormat formatter = DateFormat('yyyy-MM-dd');
+  //final String formatted = formatter.format(now);
 
 
   @override
   Widget build(BuildContext context) {
+    Offset _tapPosition;
+    void _storePosition(TapDownDetails details) {
+      _tapPosition = details.globalPosition;
+    }
     return Padding(
       padding: const EdgeInsets.only(top: 8.0),
       child: Card(
         margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
         child: ListTile(
+          //dense: true,
           leading: CircleAvatar(
             radius: 25.0,
             backgroundColor: Colors.brown,
           ),
-          title: Text(helpRequest.category.description),
-          subtitle: Text(helpRequest.date.toString()),
+          isThreeLine: true,
+          title: Text(widget.helpRequest.category.description),
+          subtitle: Container(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                //Text(widget.helpRequest.date.toString()),
+                Text(formatter.format(now)),
+                Text(widget.helpRequest.description),
+              ]
+            )
+          ),
           trailing: Row(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.call,
-                  size: 20.0,
-                  color: Colors.brown[900],
+              GestureDetector(
+                onTap: () => print("Tap: call"),
+                onLongPress: () => print("Long Press: Call"),
+                child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Icon(Icons.call,
+                        size: 20.0,
+                        color: Colors.brown[900],
+                        )
                 ),
-                onPressed: () {
-
-                },
-
               ),
-              IconButton(
-                icon: Icon(
+              GestureDetector(
+                onTap: () => print("Tap: more_vert"),
+                onTapDown: _storePosition,
+                onLongPress: () async {
+                  final RenderBox overlay =
+                  Overlay.of(context).context.findRenderObject();
+                  final int _selected = await showMenu(
+                    items: [
+                      PopupMenuItem(
+                        value: 1,
+                        child: Row(
+                          children: <Widget>[
+                            const Icon(Icons.done,
+                                      color: Colors.green,),
+                            const Text("   Accept"),
+
+                          ],
+                        ),
+                      ),
+                      PopupMenuItem(
+                        value: 2,
+                        child: Row(
+                          children: <Widget>[
+                            const Icon(Icons.clear,
+                                      color: Colors.red),
+                            const Text("   Deny"),
+                          ]
+                        )
+                      ),
+                      PopupMenuItem(
+                        value: 3,
+                        child: Row(
+                          children: <Widget>[
+                            const Icon(Icons.person),
+                            const Text("   Profile"),
+                          ],
+                        ),
+                      )
+                    ],
+                    context: context,
+                    position: RelativeRect.fromRect(
+                        _tapPosition &
+                        const Size(40, 40), // smaller rect, the touch area
+                        Offset.zero & overlay.size // Bigger rect, the entire screen
+                    ),
+                  );
+                  switch (_selected) {
+                    case 1:
+                      print("accept seleted");
+
+//                      Navigator.push(
+//                        context,
+//                        MaterialPageRoute(builder: (context) => testing_stream()),
+//                      );
+                      break;
+                    case 2:
+                      print("deny seleted");
+                      break;
+                    case 3:
+                      print("profile selected");
+                      break;
+                  }
+                },
+                child: Icon(
                   Icons.more_vert,
                   size: 20.0,
                   color: Colors.brown[900],
                 ),
-                onPressed: () {
-                  //   _onDeleteItemPressed(index);
-                },
               ),
             ],
           ),
@@ -55,97 +142,4 @@ class HelpRequestTile extends StatelessWidget {
     );
   }
 
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}): super(key:key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-
-
-  var _count = 0;
-  var _tapPosition;
-
-  void _showCustomMenu() {
-    final RenderBox overlay = Overlay.of(context).context.findRenderObject();
-
-    showMenu(
-        context: context,
-        items: <PopupMenuEntry<int>>[PlusMinusEntry()],
-        position: RelativeRect.fromRect(
-            _tapPosition & const Size(40, 40), // smaller rect, the touch area
-            Offset.zero & overlay.size   // Bigger rect, the entire screen
-        )
-    )
-    // This is how you handle user selection
-        .then<void>((int delta) {
-      // delta would be null if user taps on outside the popup menu
-      // (causing it to close without making selection)
-      if (delta == null) return;
-
-      setState(() {
-        _count = _count + delta;
-      });
-    });
-
-    // Another option:
-    //
-    // final delta = await showMenu(...);
-    //
-    // Then process `delta` however you want.
-    // Remember to make the surrounding function `async`, that is:
-    //
-    // void _showCustomMenu() async { ... }
-  }
-
-  void _storePosition(TapDownDetails details) {
-    _tapPosition = details.globalPosition;
-  }
-
-
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-class PlusMinusEntry extends PopupMenuEntry<int> {
-  @override
-  double height = 100;
-  // height doesn't matter, as long as we are not giving
-  // initialValue to showMenu().
-
-  @override
-  bool represents(int n) => n == 1 || n == -1;
-
-  @override
-  PlusMinusEntryState createState() => PlusMinusEntryState();
-}
-
-class PlusMinusEntryState extends State<PlusMinusEntry> {
-  void _plus1() {
-    // This is how you close the popup menu and return user selection.
-    Navigator.pop<int>(context, 1);
-  }
-
-  void _minus1() {
-    Navigator.pop<int>(context, -1);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(child: FlatButton(onPressed: _plus1, child: Text('+1'))),
-        Expanded(child: FlatButton(onPressed: _minus1, child: Text('-1'))),
-      ],
-    );
-  }
 }
