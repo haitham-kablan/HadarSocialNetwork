db2bff19cdec45b2938c3f8e8a7139fc2650f9f7
haitham-kablan <57827662+haitham-kablan@users.noreply.github.com>
2020-12-11 13:08:18 +0200

    just inistaed the testing

---
+++ hadar/test/data_base_test.dart
@@ -0,0 +1,19 @@
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter/material.dart';
+import 'package:hadar/feeds/helper_feed.dart';
+import 'package:hadar/services/DataBaseServices.dart';
+import 'package:hadar/users/Admin.dart';
+import 'package:hadar/users/User.dart';
+import 'package:hadar/users/UserInNeed.dart';
+import 'package:hadar/users/Volunteer.dart';
+import 'package:hadar/utils/HelpRequest.dart';
+import 'package:hadar/utils/HelpRequestType.dart';
+import 'package:provider/provider.dart';
+
+void main()  {
+
+  test('getting user test', (){});
+}
