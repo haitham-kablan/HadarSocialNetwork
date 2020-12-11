d4f04c8918907121d87395158d8c70d5953f1dc8
haitham-kablan <57827662+haitham-kablan@users.noreply.github.com>
2020-12-11 20:04:10 +0200

    depnceies and stuff

--- hadar/test/data_base_test.dart
+++ hadar/test/data_base_test.dart
@@ -1,19 +1,14 @@
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_test/flutter_test.dart';
 
+import 'package:flutter_test/flutter_test.dart';
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:hadar/feeds/helper_feed.dart';
-import 'package:hadar/services/DataBaseServices.dart';
-import 'package:hadar/users/Admin.dart';
-import 'package:hadar/users/User.dart';
-import 'package:hadar/users/UserInNeed.dart';
-import 'package:hadar/users/Volunteer.dart';
-import 'package:hadar/utils/HelpRequest.dart';
-import 'package:hadar/utils/HelpRequestType.dart';
-import 'package:provider/provider.dart';
+
+
 
 void main()  {
 
-  test('getting user test', (){});
+  test('getting user test', () {});
 }
