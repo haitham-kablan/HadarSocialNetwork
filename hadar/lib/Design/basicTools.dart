f5aad379f748430ec6f17dc57cc26a863965ddf7
HaneenJer <haneenjeries65@gmail.com>
2020-12-13 17:23:24 +0200

    top and bottom bar done

--- hadar/lib/Design/basicTools.dart
+++ hadar/lib/Design/basicTools.dart
@@ -2,12 +2,23 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
+// class BasicColor{
+//
+//   static const Color adminClr =  Color(0xC07AB8);
+// static const Color userInNeedClr =  Color(0x58C7BA);
+// static const Color HelperClr =  Color(0xF9CC8A);
+// static const Color BackgroundClr =  Color(0xE7E8EC);
+//
+// }
 class BasicColor{
 
-  static const Color adminClr =  Color(0xC07AB8);
-static const Color userInNeedClr =  Color(0x58C7BA);
-static const Color HelperClr =  Color(0xF9CC8A);
-static const Color BackgroundClr =  Color(0xE7E8EC);
+  static const Color adminClr =  Color(0xe56a7adf);
+static const Color userInNeedClr =  Color(0xe5c9968d);
+static const Color HelperClr =  Color(0xe5d07d96);
+static const Color BackgroundClr =  Color(0xffeeeff4);
+static const Color FillClr =  Color(0xffe9eaef);
+
+
 
 }
 
--- hadar/lib/main.dart
+++ hadar/lib/main.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:hadar/screens/logInScreen.dart';
 
+import 'Design/mainDesign.dart';
 
+
+class TmpClass extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(backgroundColor: Colors.transparent,);
+  }
+}
+
+class stackedPages extends StatelessWidget {
+  Widget page;
+  stackedPages(this.page);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          BackgroundDesign()
+        ],
+      ),
+    );
+  }
+}
 
 void main() {
   runApp(MaterialApp(
-    home: LogInScreen(),
+    home: stackedPages(TmpClass()),
   ));
 }
-
-
-
-
-
-
