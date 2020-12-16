9e6cefae17c6d2c5c4bb5c3f533584788a60ef53
AbuJabal-Hussein <73711735+AbuJabal-Hussein@users.noreply.github.com>
2020-12-16 18:54:05 +0200

    updated main.dart

--- hadar/lib/main.dart
+++ hadar/lib/main.dart
@@ -1,9 +1,84 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:hadar/Design/basicTools.dart';
 
-import 'Design/mainDesign.dart';
+import 'package:hadar/services/DataBaseServices.dart';
+import 'package:hadar/feeds/helper_feed.dart';
+import 'package:hadar/user_inneed_feed.dart';
+import 'package:hadar/users/User.dart';
+import 'package:hadar/utils/HelpRequest.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(
+    home: Log_In_Screen(),
+  ));
+}
+
+class Log_In_Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: BasicColor.BackgroundClr,
+        appBar: AppBar(
+          backgroundColor: BasicColor.BackgroundClr,
+          title: Text('Hadar',
+            style: TextStyle(
+              color: BasicColor.BackgroundClr,
+              fontSize: 30,
+            ),),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 200,
+            ),
+            Center(
+              child: RaisedButton(
+                child: Text("go to volunteer feed"),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HelperFeed()),
+                  );
+                },
+              ),
+            ),
+            Center(
+              child: RaisedButton(
+                child: Text("go to user in_need feed"),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) {
+                          //User(this.name, this.phoneNumber, this.email, this.privilege , this.id
+                          //       );
+                          return StreamProvider<List<HelpRequest>>.value(
+                            value: DataBaseService().getUserHelpRequests(User("haitham", "099000","no_need",Privilege.UserInNeed,"123456789")),
+                            child: UserInNeedHelpRequestsFeed(),
+                          );
+                        }
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+
+
+/*
 class TmpClass extends StatelessWidget{
   @override
   Widget build(BuildContext context){
@@ -33,3 +108,4 @@ void main() {
     home: stackedPages(TmpClass()),
   ));
 }
+*/
