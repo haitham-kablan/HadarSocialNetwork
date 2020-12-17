b466be5daf3093b89a218b4d21ba8fefe1198ea0
HaneenJer <haneenjeries65@gmail.com>
2020-12-17 06:26:42 +0200

    top and bottom bar changed

--- hadar/lib/Design/basicTools.dart
+++ hadar/lib/Design/basicTools.dart
@@ -2,18 +2,13 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-class BasicColor{
+class BasicColor {
 
-  static const Color adminClr =  Color(0xe56a7adf);
-static const Color userInNeedClr =  Color(0xe5c9968d);
-static const Color HelperClr =  Color(0xff4F9E94);
-static const Color BackgroundClr =  Color(0xffeeeff4);
-static const Color FillClr =  Color(0xffe9eaef);
-static const Color tmpClr =  Color(0xffDD0000);
-
-
-
+  static const Color clr = Color(0xff0779E9);
+  static const Color backgroundClr = Color(0xffEFEFEF);
 }
+
+
 
 
 
--- hadar/lib/main.dart
+++ hadar/lib/main.dart
@@ -3,26 +3,26 @@ import 'package:hadar/Design/basicTools.dart';
 
 import 'Design/mainDesign.dart';
 
-
-class TmpClass extends StatelessWidget{
+class TmpClass extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Scaffold(backgroundColor: BasicColor.BackgroundClr,);
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: BasicColor.backgroundClr,
+      body: Container(margin: EdgeInsets.fromLTRB(0, 250, 0, 70),),
+    );
   }
 }
 
 class stackedPages extends StatelessWidget {
   Widget page;
+
   stackedPages(this.page);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [
-          page,
-          BackgroundDesign()
-        ],
+        children: [page, BackgroundDesign()],
       ),
     );
   }
@@ -32,4 +32,12 @@ void main() {
   runApp(MaterialApp(
     home: stackedPages(TmpClass()),
   ));
+  // runApp(
+  //   MaterialApp(
+  //     home: Scaffold(
+  //       // appBar: BarDesign(BasicColor.HelperClr, 'User Feed'),
+  //       bottomNavigationBar: BottomBar(BasicColor.HelperClr),
+  //     ),
+  //   ),
+  // );
 }
