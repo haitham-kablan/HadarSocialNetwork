90af391f407d10ce58cbec471b2964c05bde514d
AbuJabal-Hussein <73711735+AbuJabal-Hussein@users.noreply.github.com>
2020-12-19 21:31:43 +0200

    fixed right to left issue

--- hadar/lib/user_inneed_feed.dart
+++ hadar/lib/user_inneed_feed.dart
@@ -229,7 +229,7 @@ class HelpRequestStatusWidget extends StatelessWidget {
                 ),
 
                 Container(
-                  padding: const EdgeInsets.only(right: 20, bottom: 20, top: 20),
+                  padding: const EdgeInsets.only(left: 20, bottom: 20, top: 20),
                   child: Align(
                     alignment: Alignment.centerRight,
                     child: Text(
