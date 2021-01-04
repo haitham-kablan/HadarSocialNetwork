5b1004626e00b4a147305e67b26bb78efd8c315e
haitham-kablan <hait.jobs@gmail.com>
2021-01-04 13:49:13 +0200

    added rating system

--- hadar/lib/users/Volunteer.dart
+++ hadar/lib/users/Volunteer.dart
@@ -12,13 +12,19 @@ import 'User.dart';
 class Volunteer extends RegisteredUser{
   List<HelpRequestType> helpRequestsCategories;
   List helpRequests;
+  int count;
+  int stars;
 
-  Volunteer(String name, String phoneNumber, String email, bool isSignedIn , String id , List<HelpRequestType> categories)
+  Volunteer(String name, String phoneNumber, String email, bool isSignedIn , String id , List<HelpRequestType> categories, int stars , int count)
       : super(name, phoneNumber, email, Privilege.Volunteer, isSignedIn , id){
 
     helpRequestsCategories = categories;
     helpRequests = new List<HelpRequest>();
+    this.count  = count;
+    this.stars = stars;
   }
+
+
 
   void addOffer(){} //todo: implement this
 }
