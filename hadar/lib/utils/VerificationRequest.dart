5c90c38ae0fdc081bdb2b2e99609f484108415f4
haitham-kablan <hait.jobs@gmail.com>
2021-01-13 11:46:49 +0200

    fixed verif request bug

--- hadar/lib/utils/VerificationRequest.dart
+++ hadar/lib/utils/VerificationRequest.dart
@@ -24,6 +24,18 @@ class VerificationRequest{
     String location = '',String status = '',String work = '',String birthplace = '',String spokenlangs = '' , String firstaidcourse = '' ,String mobility = ''}) {
     accepted = false;
     time = date.millisecondsSinceEpoch;
+    this.sender = sender;
+    this.status = status;
+    this.firstaidcourse = firstaidcourse;
+    this.mobility = mobility;
+    this.spokenlangs = spokenlangs;
+    this.birthplace = birthplace;
+    this.birthdate = birthdate;
+    this.location = location;
+    this.work = work;
+    this.type = type;
+    this.date = date;
+
   }
 
 
