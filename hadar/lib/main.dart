8cd4dfddf9e5ff3d4670d3b5d4fecef1fd75e783
haitham-kablan <hait.jobs@gmail.com>
2021-04-20 12:36:44 +0300

    now if a user sigend in , he remain signed in until he log out

--- hadar/lib/main.dart
+++ hadar/lib/main.dart
@@ -8,6 +8,7 @@ import 'package:hadar/services/DataBaseServices.dart';
 
 import 'package:hadar/services/authentication/LogInPage.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:hadar/users/CurrentUser.dart';
 
 import 'package:hadar/utils/HelpRequest.dart';
 import 'package:hadar/utils/HelpRequestType.dart';
@@ -18,12 +19,13 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   const double marginSize = kIsWeb ? 0.0 : 0.0;
+  Widget logged_in_user = await CurrentUser.init_user();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Container(
         margin: const EdgeInsets.only(left: marginSize, right: marginSize),
         child: Scaffold(
-          body: LogInPage(),
+          body: logged_in_user != null ? logged_in_user : LogInPage(),
         ),
     ),
   ));
