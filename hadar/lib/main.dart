36692d9ec7d8e51b771137e1b098e70d4888de0e
haitham-kablan <hait.jobs@gmail.com>
2021-06-23 01:19:27 +0300

    Update main.dart

--- hadar/lib/main.dart
+++ hadar/lib/main.dart
@@ -59,7 +59,7 @@ class _MainAppState extends State<MainApp> {
   Widget build(BuildContext context) {
     const double marginSize = kIsWeb ? 0.0 : 0.0;
     _fetchUserLocal();
-    print("s");
+    print("ss");
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       localizationsDelegates: [
